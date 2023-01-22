8d26454b02aec52d148683a7e065071bc70289f0
Duex <shangqinglong@DuexMacBook-Air.local>
2023-01-22 23:26:35 +0800

    添加守卫

--- src/common/auth/auth.guard.ts
+++ src/common/auth/auth.guard.ts
@@ -1,16 +1,16 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  NotAcceptableException,
+} from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { FastifyRequest } from 'fastify';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
   private whiteList = ['', 'auth'];
-  private inCludeWhiteList(url: string, whiteList: string[]) {
-    const pathSplit = url.split('/');
-    pathSplit.shift();
-    const result = pathSplit.some((clip) => whiteList.includes(clip));
-    return result;
-  }
+
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
@@ -21,7 +21,25 @@ export class AuthGuard implements CanActivate {
       console.log('白名单');
       return true;
     }
+    const token = request.headers.token;
+    const tokenAble = this.validateJWT(token);
+    if (!tokenAble) {
+      throw new NotAcceptableException('Not Login');
+    }
     // if(this.whiteList.includes())
+    return true;
+  }
+
+  private inCludeWhiteList(url: string, whiteList: string[]) {
+    const pathSplit = url.split('/');
+    pathSplit.shift();
+    const result = pathSplit.some((clip) => whiteList.includes(clip));
+    return result;
+  }
+
+  private validateJWT(token: string | string[]) {
+    if (!token || !token.length) return false;
+    // token验证
     return true;
   }
 }
