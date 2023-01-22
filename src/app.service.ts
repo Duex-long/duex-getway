a0151a4cd636609f6212898b50c7ec3a8fb21d88
Duex <shangqinglong@DuexMacBook-Air.local>
2023-01-22 23:07:45 +0800

    同步入口文件

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  HttpException,
+  HttpStatus,
+  Injectable,
+  PayloadTooLargeException,
+  RequestTimeoutException,
+} from '@nestjs/common';
 
 @Injectable()
 export class AppService {
   getHello(): string {
+    // throw Error('test')
+    // throw new HttpException('testError', HttpStatus.SERVICE_UNAVAILABLE);
+    // throw new PayloadTooLargeException('large', 'testLarge');
     return 'Hello World!';
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
+import { AuthGuard } from './common/auth/auth.guard';
+import { AllExceptionsFilter } from './common/exceptions/base.exceptions';
+import TransformIntercetp from './common/intercept/transform.intercetp';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+  app.useGlobalInterceptors(new TransformIntercetp());
+  app.useGlobalFilters(new AllExceptionsFilter());
+  app.useGlobalGuards(new AuthGuard());
   await app.listen(3000);
 }
 bootstrap();
