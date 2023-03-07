ae26db2a7db69c34e167630d8902baa0e62fc189
Shangqinglong <long.shangqing@gempoll.com>
2023-03-07 18:51:18 +0800

    article实体

---
+++ src/common/db/mysql/entity/article.mysql.entity.ts
@@ -0,0 +1,34 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class Article {
+  constructor() {
+    console.log('Article表注入');
+  }
+
+  /**自增主键 */
+  @PrimaryGeneratedColumn()
+  id?: number;
+
+  /**所属分类 */
+  @Column({ default: '' })
+  sort?: string;
+
+  /**内容 */
+  @Column({ type: 'longtext', default: '' })
+  content: string;
+
+  /**创建时间 */
+  @CreateDateColumn()
+  createDate?: string;
+
+  /**更新时间 */
+  @UpdateDateColumn()
+  updateDate?: string;
+}
